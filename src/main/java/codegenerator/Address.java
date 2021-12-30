3c7dfc67f7a42f38ab1989886adf3a8aa12c8942
fforghani <fereshteh.forghani2012@gmail.com>
2021-12-30 22:27:41 +0330

    10- Eliminating switch statement by adding string values to the TypeAddress enum.

--- src/main/java/codegenerator/Address.java
+++ src/main/java/codegenerator/Address.java
@@ -20,14 +20,6 @@ public class Address {
         this.varType = varType;
     }
     public String toString(){
-        switch (Type){
-            case Direct:
-                return num+"";
-            case Indirect:
-                return "@"+num;
-            case Imidiate:
-                return "#"+num;
-        }
-        return num+"";
+        return Type.label + num;
     }
 }
--- src/main/java/codegenerator/TypeAddress.java
+++ src/main/java/codegenerator/TypeAddress.java
@@ -4,7 +4,13 @@ package codegenerator;
  * Created by mohammad hosein on 6/28/2015.
  */
 public enum TypeAddress {
-    Direct,
-    Indirect,
-    Imidiate
+    Direct (""),
+    Indirect ("@"),
+    Imidiate ("#");
+
+    public final String label;
+
+    TypeAddress(String label) {
+        this.label = label;
+    }
 }
